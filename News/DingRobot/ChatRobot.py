29fecb271cc33868773d51df32a53296df2d06c3
2938234632@qq.com <2938234632@qq.com>
2019-08-05 17:33:08 +0800

    feat:封装定义钉钉机器人发送消息类型操作

---
+++ News/DingRobot/ChatRobot.py
@@ -0,0 +1,149 @@
+import requests
+import re
+import urllib.request
+import json
+import sys
+import os
+import time
+import logging
+
+try:
+    JSONDecodeError = json.decoder.JSONDecodeError
+except AttributeError:
+    JSONDecodeError = ValueError
+
+
+def is_not_null_and_blank_str(content):
+    """
+    非空字符串
+    :param content: 字符串
+    :return: 非空 - True，空 - False
+    >>> is_not_null_and_blank_str('')
+    False
+    >>> is_not_null_and_blank_str(' ')
+    False
+    >>> is_not_null_and_blank_str('  ')
+    False
+    >>> is_not_null_and_blank_str('123')
+    True
+    """
+    if content and content.strip():
+        return True
+    else:
+        return False
+
+class DingtalkChatbot(object):
+    """
+    钉钉群自定义机器人（每个机器人每分钟最多发送20条），支持文本（text）、连接（link）、markdown三种消息类型！
+    """
+    def __init__(self, webhook):
+        """
+        机器人初始化
+        :param webhook: 钉钉群自定义机器人webhook地址
+        """
+        super(DingtalkChatbot, self).__init__()
+        self.headers = {'Content-Type': 'application/json; charset=utf-8'}
+        self.webhook = webhook
+        self.times = 0
+        self.start_time = time.time()
+
+    def send_feed_card(self, links):
+        """
+        FeedCard类型
+        :param links: 信息集（FeedLink数组）
+        :return: 返回消息发送结果
+        """
+        link_data_list = []
+        for link in links:
+            if isinstance(link, FeedLink):
+                link_data_list.append(link.get_data())
+        if link_data_list:
+            # 兼容：1、传入FeedLink或CardItem实例列表；2、传入数据字典列表；
+            links = link_data_list
+        data = {"msgtype": "feedCard", "feedCard": {"links": links}}
+        logging.debug("FeedCard类型：%s" % data)
+        return self.post(data)
+
+    def post(self, data):
+        """
+        发送消息（内容UTF-8编码）
+        :param data: 消息数据（字典）
+        :return: 返回发送结果
+        """
+        self.times += 1
+        if self.times % 20 == 0:
+            if time.time() - self.start_time < 60:
+                logging.debug('钉钉官方限制每个机器人每分钟最多发送20条，当前消息发送频率已达到限制条件，休眠一分钟')
+                time.sleep(60)
+            self.start_time = time.time()
+
+        post_data = json.dumps(data)
+        try:
+            response = requests.post(self.webhook, headers=self.headers, data=post_data)
+        except requests.exceptions.HTTPError as exc:
+            logging.error("消息发送失败， HTTP error: %d, reason: %s" % (exc.response.status_code, exc.response.reason))
+            raise
+        except requests.exceptions.ConnectionError:
+            logging.error("消息发送失败，HTTP connection error!")
+            raise
+        except requests.exceptions.Timeout:
+            logging.error("消息发送失败，Timeout error!")
+            raise
+        except requests.exceptions.RequestException:
+            logging.error("消息发送失败, Request Exception!")
+            raise
+        else:
+            try:
+                result = response.json()
+            except JSONDecodeError:
+                logging.error("服务器响应异常，状态码：%s，响应内容：%s" % (response.status_code, response.text))
+                return {'errcode': 500, 'errmsg': '服务器响应异常'}
+            else:
+                logging.debug('发送结果：%s' % result)
+                if result['errcode']:
+                    error_data = {"msgtype": "text", "text": {"content": "钉钉机器人消息发送失败，原因：%s" % result['errmsg']},
+                                  "at": {"isAtAll": True}}
+                    logging.error("消息发送失败，自动通知：%s" % error_data)
+                    requests.post(self.webhook, headers=self.headers, data=json.dumps(error_data))
+                return result
+
+
+class FeedLink(object):
+    """
+    FeedCard类型单条消息格式
+    """
+    def __init__(self, title, message_url, pic_url):
+        """
+        初始化单条消息文本
+        :param title: 单条消息文本
+        :param message_url: 点击单条信息后触发的URL
+        :param pic_url: 点击单条消息后面图片触发的URL
+        """
+        super(FeedLink, self).__init__()
+        self.title = title
+        self.message_url = message_url
+        self.pic_url = pic_url
+
+    def get_data(self):
+        """
+        获取FeedLink消息数据（字典）
+        :return: 本FeedLink消息的数据
+        """
+        if is_not_null_and_blank_str(self.title) and is_not_null_and_blank_str(self.message_url) and is_not_null_and_blank_str(self.pic_url):
+            data = {
+                    "title": self.title,
+                    "messageURL": self.message_url,
+                    "picURL": self.pic_url
+            }
+            return data
+        else:
+            logging.error("FeedCard类型单条消息文本、消息链接、图片链接不能为空！")
+            raise ValueError("FeedCard类型单条消息文本、消息链接、图片链接不能为空！")
+
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
+
